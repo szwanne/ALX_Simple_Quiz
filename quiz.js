965b36e7be370fa339e7eb1301be73cd2084d63c
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-13 22:32:34 +0200

    Initial commit

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function checkAnswer() {
   const correctAnswer = "4";
   const userAnswer = document.querySelector("input[name='quiz']:checked");
 
-  if (userAnswer === correctAnswer) {
+  if (userAnswer && userAnswer.value === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
