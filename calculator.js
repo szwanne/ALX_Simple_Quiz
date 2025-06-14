9c4141e70881da0b8a954f333c928fce4cd8c711
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-14 09:18:22 +0200

    Added event listener to all the maths operations

--- calculator.js
+++ calculator.js
@@ -17,3 +17,31 @@ function division(number1, number2) {
     return number1 / number2;
   }
 }
+
+document.getElementById("add").addEventListener("click", function () {
+  const number1 = parseFloat(document.getElementById("number1").value) || 0;
+  const number2 = parseFloat(document.getElementById("number2").value) || 0;
+  const result = add(number1, number2);
+  document.getElementById("calculation-result").textContent = result;
+});
+
+document.getElementById("subtract").addEventListener("click", function () {
+  const number1 = parseFloat(document.getElementById("number1").value) || 0;
+  const number2 = parseFloat(document.getElementById("number2").value) || 0;
+  const result = subtraction(number1, number2);
+  document.getElementById("calculation-result").textContent = result;
+});
+
+document.getElementById("multiply").addEventListener("click", function () {
+  const number1 = parseFloat(document.getElementById("number1").value) || 0;
+  const number2 = parseFloat(document.getElementById("number2").value) || 0;
+  const result = multiplication(number1, number2);
+  document.getElementById("calculation-result").textContent = result;
+});
+
+document.getElementById("divide").addEventListener("click", function () {
+  const number1 = parseFloat(document.getElementById("number1").value) || 0;
+  const number2 = parseFloat(document.getElementById("number2").value) || 0;
+  const result = division(number1, number2);
+  document.getElementById("calculation-result").textContent = result;
+});
